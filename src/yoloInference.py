8989eef2c581f7ffb5a38ebdf405073456693d14
Francisco Kim <ratonwasher@gmail.com>
2026-05-04 14:29:59 +0200

    Modify inference script after training

--- src/yoloInference.py
+++ src/yoloInference.py
@@ -1,8 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO('yolo11x.pt')
+#model = YOLO('yolo11x.pt')
+#model = YOLO('./src/models/best_yolo5x.pt')
+model = YOLO('./src/models/best_yolo11s.pt')
 
-results = model.predict('./data/Bundesliga.mp4', stream=True, save=True)
+results = model.predict('./data/Bundesliga.mp4', save=True)
 
 # First frame of the video
 firstFrame = results[0]
