cb4f985835b4c869c0ce3f859bdbe59872dcfa32
Francisco Kim <ratonwasher@gmail.com>
2026-05-05 10:45:34 +0200

    Team clustering with k-means

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
 from utils import ReadVideo, SaveVideo
 from trackers import Tracker
+import cv2
+
+def GetCroppedImageOfPlayer(videoFrames, tracks):
+    for trackID, player in tracks['players'][0].items():
+        boundingBox = player['bbox']
+        frame = videoFrames[0]
+
+        # Crop boundingBox from frame
+        croppedImage = frame[int(boundingBox[1]):int(boundingBox[3]), int(boundingBox[0]):int(boundingBox[2])]
+        cv2.imwrite(f'data/croppedPlayer.jpg', croppedImage)
+        break
+
+    print(f'Image saved.')
 
 def main():
     # Read video
@@ -8,6 +21,9 @@ def main():
     # Initialise Tracker
     tracker = Tracker('./src/models/best_yolo11s.pt')
     tracks = tracker.GetObjectTracks(videoFrames, read_from_data=True, data_path='./data/track_data.pkl')
+
+    # Save cropped image of a player for clustering
+    #GetCroppedImageOfPlayer(videoFrames, tracks)
 
     # Draw output
     ## Draw object Tracks
